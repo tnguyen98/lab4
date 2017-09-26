f22fcc5dfc3d4ad273a7900541efbec9f8769be2
Timothy Nguyen <tnguyen7@ufl.edu>
2017-09-26 17:52:31 -0400

    Sample commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner scnr = new Scanner(System.in);
+        int randomInt;
+        System.out.println("Enter an integer: ");
+        randomInt = scnr.nextInt();
+        System.out.println("The integer is: " + randomInt);
+        return;
+    }
+}
